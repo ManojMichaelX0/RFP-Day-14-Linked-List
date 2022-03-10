e0d15332812cdd278db3b0cf30d355fddf01a5db
ManojMichaelX0 <manojthiparapu@gmail.com>
2022-03-11 04:51:41 +0530

    added UseCase-3- Append values

--- Day-14-Linked-List-Practice-Problems/Program.cs
+++ Day-14-Linked-List-Practice-Problems/Program.cs
@@ -9,9 +9,10 @@ namespace Day_14_Linked_List_Practice_Problems
             Console.WriteLine("Welcome to Linkedlist : operations");
             LinkedList list = new LinkedList();
             list.Add(56);
-            list.Add(30);
-            list.Add(40);
-            list.Add(70);
+            int a = list.Search(56);
+            list.InsertAtParticulatPosition(a+1,30);
+            int b = list.Search(30);
+            list.InsertAtParticulatPosition(b + 1, 70);
             list.Display();
             //Console.WriteLine("Popping first Element in List");
             //list.PoP_First_element();
@@ -23,11 +24,11 @@ namespace Day_14_Linked_List_Practice_Problems
             //Console.WriteLine("index position of element 30 is :" + a);
             //list.InsertAtParticulatPosition(a + 1, 40);
             //list.Display();
-            Console.WriteLine("Popping element 40 in List");
-            int b = list.Search(40);
-            Console.WriteLine("index position of element 40 is :" + b);
-            list.PoP_Paricular_element(3);
-            list.Display();
+            //Console.WriteLine("Popping element 40 in List");
+            //int b = list.Search(40);
+            //Console.WriteLine("index position of element 40 is :" + b);
+            //list.PoP_Paricular_element(3);
+            //list.Display();
 
         }
     }

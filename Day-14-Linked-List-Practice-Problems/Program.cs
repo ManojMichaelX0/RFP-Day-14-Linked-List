593f358c68a060d5b57f8e42476ed3979388f375
ManojMichaelX0 <manojthiparapu@gmail.com>
2022-03-11 03:04:44 +0530

    added UseCase-1-Linked-List problem

---
+++ Day-14-Linked-List-Practice-Problems/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Day_14_Linked_List_Practice_Problems
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Linkedlist : operations");
+            LinkedList list = new LinkedList();
+            list.Add(56);
+            list.Add(30);
+            list.Add(70);
+            list.Display();
+
+        }
+    }
+}
